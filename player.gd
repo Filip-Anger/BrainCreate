23faa2e62cd888fe2ced7bf1ec24a3ea48a7375c
FilipAnger <filip.anger@gmail.com>
2026-01-23 19:45:54 +0100

    Colisions working

--- player.gd
+++ player.gd
@@ -1,19 +1,21 @@
 extends CharacterBody2D
 @export var movmentSpeed = 400
-
+var animatedSprite;
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+	animatedSprite = $Player
 	pass # Replace with function body.
 
 func get_input():
-	var input_diretion = Input.get_vector('Left', 'Right', 'w', 's')
-	return input_diretion
+	return Input.get_vector('Left', 'Right', 'w', 's')
 
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var inputVect = get_input()
-	position += inputVect
-	
+	animatedSprite.animation(inputVect)
+	move_and_collide(inputVect)
+	if move_and_slide():
+		# Play hit sound
+		pass
 	
